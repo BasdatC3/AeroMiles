03596a68115c2cefa2ae3a59182f01d623db7fa3
qowiys <qowiyshabir1416@gmail.com>
2026-04-30 22:46:57 +0700

    Add CRUD for Claim Miles, Claim Approval, & Transfer Miles

--- features/transactions/views.py
+++ features/transactions/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.http import JsonResponse
-from django.views.decorators.http import require_POST
+from django.db import connection
 
 
 def get_user_from_request(request):
@@ -9,32 +8,65 @@ def get_user_from_request(request):
     user_role = request.session.get('user_role')
     return user_email, user_role
 
-
 def claim_miles(request):
-    """Klaim Miles - untuk Member"""
     user_email, role = get_user_from_request(request)
     
     from features.accounts.models import ClaimMissingMiles
+    if request.method == "POST":
+        flight_number = request.POST.get('flight_number')
+
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                INSERT INTO CLAIM_MISSING_MILES 
+                (email_member, maskapai, bandara_asal, bandara_tujuan, tanggal_penerbangan, flight_number, nomor_tiket, kelas_kabin, pnr)
+                VALUES (%s, 'GA', 'CGK', 'DPS', CURRENT_DATE, %s, 'AUTO', 'Economy', 'AUTO')
+            """, [user_email, flight_number])
+
+        messages.success(request, "Klaim berhasil diajukan!")
+        return redirect('claim_miles')
+
     claims = ClaimMissingMiles.objects.filter(email_member=user_email).order_by('-timestamp')
 
     return render(request, 'claim_miles.html', {'claims': claims})
 
-
 def transfer_miles(request):
-    """Transfer Miles - untuk Member"""
     user_email, role = get_user_from_request(request)
     
     from features.accounts.models import TransferMiles
+    if request.method == "POST":
+        to_email = request.POST.get('to_email')
+        jumlah = int(request.POST.get('jumlah'))
+
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                UPDATE MEMBER
+                SET award_miles = award_miles - %s
+                WHERE email = %s
+            """, [jumlah, user_email])
+
+            cursor.execute("""
+                UPDATE MEMBER
+                SET award_miles = award_miles + %s
+                WHERE email = %s
+            """, [jumlah, to_email])
+
+            cursor.execute("""
+                INSERT INTO TRANSFER (email_member_1, email_member_2, jumlah, catatan)
+                VALUES (%s, %s, %s, 'Transfer 1')
+            """, [user_email, to_email, jumlah])
+
+        messages.success(request, "Transfer berhasil!")
+        return redirect('transfer_miles')
+
     transfers = TransferMiles.objects.filter(email_member_1=user_email).order_by('-timestamp')
 
     return render(request, 'transfer_miles.html', {'transfers': transfers})
 
-
 def manage_claims(request):
-    """Kelola Klaim - untuk Staf"""
     user_email, role = get_user_from_request(request)
     
     from features.accounts.models import ClaimMissingMiles
+
     claims = ClaimMissingMiles.objects.all().order_by('-timestamp')
     status_filter = request.GET.get('status', '')
 
@@ -46,9 +78,33 @@ def manage_claims(request):
         'status_filter': status_filter,
     })
 
+def approve_claim(request, id):
+    user_email, _ = get_user_from_request(request)
+
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            UPDATE CLAIM_MISSING_MILES
+            SET status_penerimaan = 'Disetujui',
+                email_staf = %s
+            WHERE id = %s
+        """, [user_email, id])
+
+    return redirect('manage_claims')
+
+def reject_claim(request, id):
+    user_email, _ = get_user_from_request(request)
+
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            UPDATE CLAIM_MISSING_MILES
+            SET status_penerimaan = 'Ditolak',
+                email_staf = %s
+            WHERE id = %s
+        """, [user_email, id])
+
+    return redirect('manage_claims')
 
 def manage_partners(request):
-    """Kelola Mitra - untuk Staf"""
     user_email, role = get_user_from_request(request)
     
     from features.accounts.models import Mitra
@@ -58,7 +114,6 @@ def manage_partners(request):
 
 
 def transaction_report(request):
-    """Laporan Transaksi - untuk Staf"""
     user_email, role = get_user_from_request(request)
     
     from features.accounts.models import Member, ClaimMissingMiles, Redeem
