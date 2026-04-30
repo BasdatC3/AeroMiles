332220f5827b9d5937b964702720ddb1a4e8ce8a
qowiys <qowiyshabir1416@gmail.com>
2026-05-01 00:16:44 +0700

    fix urls error

--- features/transactions/urls.py
+++ features/transactions/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('manage-claims/', views.manage_claims, name='manage_claims'),
     path('claims/approve/<int:id>/', views.approve_claim, name='approve_claim'),
     path('claims/reject/<int:id>/', views.reject_claim, name='reject_claim'),
-    path('manage-partners/', views.manage_partners, name='manage_partners'),
     path('report/', views.transaction_report, name='transaction_report'),
 ]
